ca83d8ebff6af5ac93e7db3887de96acf7813934
Wandonium <hillarywando@gmail.com>
2019-07-19 11:36:31 +0300

    change config variable from DB_URL to DATABASE_URL

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class Development(object):
   DEBUG = True
   TESTING = False
   JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
   UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
 
 class Production(object):
@@ -16,7 +16,7 @@ class Production(object):
   """
   DEBUG = False
   TESTING = False
-  SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
   JWT_SECRECT_KEY = os.getenv('JWT_SECRET_KEY')
   UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
 
@@ -25,7 +25,7 @@ class Testing(object):
   Testing environment configuration
   """
   TESTING = True
-  SQLALCHEMY_DATABASE_URI = os.getenv('DB_TEST_URL')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
   JWT_SECRECT_KEY = os.getenv('JWT_SECRET_KEY')
   UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
